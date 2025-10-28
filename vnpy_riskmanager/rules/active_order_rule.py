6d73de60df5310863d9371d8ae2989814ec53e38
vn.py <xiaoyou.chen@foxmail.com>
2025-10-28 13:08:27 +0800

    [Mod] 重构活动委托和报撤笔数风控规则

--- vnpy_riskmanager/rules/active_order_rule.py
+++ vnpy_riskmanager/rules/active_order_rule.py
@@ -1,15 +1,10 @@
-from typing import TYPE_CHECKING
-
 from vnpy.trader.object import OrderRequest, OrderData
 
 from ..template import RuleTemplate
 
-if TYPE_CHECKING:
-    from ..engine import RiskEngine
-
 
 class ActiveOrderRule(RuleTemplate):
-    """活动委托数量上限"""
+    """活动委托数量风控规则"""
 
     name: str = "活动委托风控"
 
@@ -18,29 +13,35 @@ class ActiveOrderRule(RuleTemplate):
     }
 
     variables: dict[str, str] = {
-        "_active_order_count": "当前活动委托"
+        "active_order_count": "活动委托数量"
     }
 
-    def __init__(self, risk_engine: "RiskEngine", setting: dict) -> None:
-        super().__init__(risk_engine, setting)
+    def on_init(self) -> None:
+        """初始化"""
+        # 默认参数
+        self.active_order_limit: int = 50
 
-        self.active_order_limit: int = 10
+        # 活动委托
+        self.active_orders: dict[str, OrderData] = {}
 
-        self._active_orders: dict[str, OrderData] = {}
-        self._active_order_count: int = 0
+        # 数量统计
+        self.active_order_count: int = 0
 
     def check_allowed(self, req: OrderRequest, gateway_name: str) -> bool:
         """检查是否允许委托"""
-        if self._active_order_count >= self.active_order_limit:
-            self.write_log(f"活动委托数量{self._active_order_count}达到上限{self.active_order_limit}")
+        if self.active_order_count >= self.active_order_limit:
+            self.write_log(f"活动委托数量{self.active_order_count}达到上限{self.active_order_limit}")
             return False
+
         return True
 
     def on_order(self, order: OrderData) -> None:
-        """"""
+        """委托推送"""
         if order.is_active():
-            self._active_orders[order.vt_orderid] = order
-        elif order.vt_orderid in self._active_orders:
-            self._active_orders.pop(order.vt_orderid)
+            self.active_orders[order.vt_orderid] = order
+        elif order.vt_orderid in self.active_orders:
+            self.active_orders.pop(order.vt_orderid)
 
-        self._active_order_count = len(self._active_orders)
+        self.active_order_count = len(self.active_orders)
+
+        self.put_event()
---
+++ vnpy_riskmanager/rules/trading_count_rule.py
@@ -0,0 +1,73 @@
+from collections import defaultdict
+
+from vnpy.trader.object import OrderRequest, OrderData
+from vnpy.trader.constant import OrderStatus
+
+from ..template import RuleTemplate
+
+
+class TradingCountRule(RuleTemplate):
+    """报撤笔数风控"""
+
+    name: str = "报撤笔数风控"
+
+    parameters: dict[str, str] = {
+        "total_order_limit": "全天委托上限",
+        "total_cancel_limit": "全天撤单上限"
+    }
+
+    variables: dict[str, str] = {
+        "total_order_count": "全天委托笔数",
+        "total_cancel_count": "全天撤单笔数",
+        "contract_order_count": "合约委托笔数",
+        "contract_cancel_count": "合约撤单笔数"
+    }
+
+    def on_init(self) -> None:
+        """初始化"""
+        # 默认参数
+        self.total_order_limit: int = 1000
+        self.total_cancel_limit: int = 800
+
+        # 委托号记录
+        self.all_orderids: set[str] = set()
+        self.cancel_orderids: set[str] = set()
+
+        # 数量统计
+        self.total_order_count: int = 0
+        self.total_cancel_count: int = 0
+
+        self.contract_order_count: dict[str, int] = defaultdict(int)
+        self.contract_cancel_count: dict[str, int] = defaultdict(int)
+
+    def check_allowed(self, req: OrderRequest, gateway_name: str) -> bool:
+        """检查是否允许委托"""
+        if self.total_order_count >= self.total_order_limit:
+            self.write_log(f"全天委托笔数{self.total_order_count}达到上限{self.total_order_limit}")
+            return False
+
+        if self.total_cancel_count >= self.total_cancel_limit:
+            self.write_log(f"全天撤单笔数{self.total_cancel_count}达到上限{self.total_cancel_limit}")
+            return False
+
+        return True
+
+    def on_order(self, order: OrderData) -> None:
+        """委托推送"""
+        if order.vt_orderid not in self.all_orderids:
+            self.all_orderids.add(order.vt_orderid)
+            self.total_order_count += 1
+
+            self.contract_order_count[order.vt_symbol] += 1
+
+            self.put_event()
+        elif (
+            order.status == OrderStatus.CANCELLED
+            and order.vt_orderid not in self.cancel_orderids
+        ):
+            self.cancel_orderids.add(order.vt_orderid)
+            self.total_cancel_count += 1
+
+            self.contract_cancel_count[order.vt_symbol] += 1
+
+            self.put_event()
